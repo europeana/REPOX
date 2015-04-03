47949885e73a534f80bf622098d79205704c80e3
stzanakis <Simon.Tzanakis@theeuropeanlibrary.org>
2015-04-03 13:57:37 +0200

    Complete AggregatorAccessor with tests and logs

--- client/client-rest-jersey/src/main/java/eu/europeana/repox/rest/client/accessors/AggregatorAccessor.java
+++ client/client-rest-jersey/src/main/java/eu/europeana/repox/rest/client/accessors/AggregatorAccessor.java
@@ -12,11 +12,16 @@
  * the Licence.
  */
 package eu.europeana.repox.rest.client.accessors;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -28,7 +33,10 @@ import org.theeuropeanlibrary.repox.rest.pathOptions.AggregatorOptionListContain
 import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
 
 import pt.utl.ist.dataProvider.Aggregator;
+import pt.utl.ist.util.exceptions.AlreadyExistsException;
 import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+import pt.utl.ist.util.exceptions.MissingArgumentsException;
 
 /**
  * @author Simon Tzanakis (Simon.Tzanakis@theeuropeanlibrary.org)
@@ -42,66 +50,215 @@ public class AggregatorAccessor {
 
   /**
    * Setup AggregatorAccessor with the target Url, username and password
+   * 
    * @param restUrl
    * @param username
    * @param passwordS
-   * @throws MalformedURLException 
+   * @throws MalformedURLException
    */
-  public AggregatorAccessor(URL restUrl, String username, String password) throws MalformedURLException {
+  public AggregatorAccessor(URL restUrl, String username, String password)
+      throws MalformedURLException {
     super();
-    
-    this.restUrl = restUrl.toString().charAt(restUrl.toString().length() -1) == '/' ? new URL(restUrl.toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
+
+    this.restUrl =
+        restUrl.toString().charAt(restUrl.toString().length() - 1) == '/' ? new URL(restUrl
+            .toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
     HttpAuthenticationFeature authfeature = HttpAuthenticationFeature.basic(username, password);
     client.register(authfeature);
+    LOGGER.info("AggregatorAccessor created with target url: {}", this.restUrl);
   }
-  
+
   /**
-   * Only used for tests.
-   * Setup AggregatorAccessor with the target Url, username and password
+   * Only used for tests. Setup AggregatorAccessor with the target Url, username and password
+   * 
    * @param restUrl
    * @param username
    * @param password
    * @param target
-   * @throws MalformedURLException 
+   * @throws MalformedURLException
    */
-  public AggregatorAccessor(URL restUrl, String username, String password, Client client) throws MalformedURLException {
+  AggregatorAccessor(URL restUrl, String username, String password, Client client)
+      throws MalformedURLException {
     super();
-    this.restUrl = restUrl.toString().charAt(restUrl.toString().length() -1) == '/' ? new URL(restUrl.toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
+    this.restUrl =
+        restUrl.toString().charAt(restUrl.toString().length() - 1) == '/' ? new URL(restUrl
+            .toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
     this.client = client;
     HttpAuthenticationFeature authfeature = HttpAuthenticationFeature.basic(username, password);
     client.register(authfeature);
   }
-  
-  
-  public static void main(String[] args) throws DoesNotExistException, MalformedURLException {
-  AggregatorAccessor aa = new AggregatorAccessor(new URL("http://localhost:8080/repox/rest/"), "temporary", "temporary");
-  Aggregator aggregator = aa.getAggregator("A0r0");
-//  
-//  System.out.println(aggregator.getId());
-  
-  
- 
+
+  public static void main(String[] args) throws DoesNotExistException, MalformedURLException,
+      InvalidArgumentsException, InternalServerErrorException, MissingArgumentsException,
+      AlreadyExistsException {
+    AggregatorAccessor aa =
+        new AggregatorAccessor(new URL("http://localhost:8080/repox/rest/"), "temporary",
+            "temporary");
+    LOGGER.debug("OK");
+    aa.updateAggregator("ValidId", "ValidId2", "Greeces", "GR", "http://example.com");
+    LOGGER.debug("OK");
   }
 
   /**
-   * Retrieve an Aggregator by id.
+   * Retrieve the aggregator with the provided id.
+   * 
    * @param aggregatorId
    * @return Aggregator
-   * @throws DoesNotExistException 
+   * @throws DoesNotExistException
    */
   public Aggregator getAggregator(String aggregatorId) throws DoesNotExistException {
-    WebTarget target = client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS + "/"+ aggregatorId);
+    WebTarget target =
+        client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS + "/"
+            + aggregatorId);
     Response response = target.request(MediaType.APPLICATION_JSON).get();
-    if(response.getStatus() == 404)
-    {
+    if (response.getStatus() == 404) {
       Result errorMessage = response.readEntity(Result.class);
-      LOGGER.warn("getAggregator() failure! : " + errorMessage.getResult());
+      LOGGER.warn("getAggregator(..) failure! : " + errorMessage.getResult());
       throw new DoesNotExistException(errorMessage.getResult());
     }
     Aggregator aggregator = response.readEntity(Aggregator.class);
-    LOGGER.info("getAggregator() success!");
+    LOGGER.info("getAggregator(..) success!");
 
     return aggregator;
+  }
+
+  /**
+   * Delete an aggregator by specifying the Id.
+   * 
+   * @param aggregatorId
+   * @throws DoesNotExistException
+   */
+  public void deleteAggregator(String aggregatorId) throws DoesNotExistException,
+      InternalServerErrorException {
+    WebTarget target =
+        client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS + "/"
+            + aggregatorId);
+    Response response = target.request(MediaType.APPLICATION_JSON).delete();
+    if (response.getStatus() == 404) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("deleteAggregator(..) failure! : " + errorMessage.getResult());
+      throw new DoesNotExistException(errorMessage.getResult());
+    } else if (response.getStatus() == 500) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("deleteAggregator(..) failure! : " + errorMessage.getResult());
+      throw new InternalServerErrorException(errorMessage.getResult());
+    }
+    LOGGER.info("deleteAggregator(..) success!");
+  }
+
+  /**
+   * Get a list of aggregators in the specified range. Returned number can be smaller than the
+   * requested. Offset not allowed negative. If number is negative then it returns all the items
+   * from offset until the total number of items.
+   * 
+   * @param offset
+   * @param number
+   * @return the list of the number of aggregators requested
+   * @throws InvalidArgumentsException
+   */
+  public List<Aggregator> getAggregatorList(int offset, int number)
+      throws InvalidArgumentsException {
+    WebTarget target =
+        client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS)
+            .queryParam(AggregatorOptionListContainer.OFFSET, offset)
+            .queryParam(AggregatorOptionListContainer.NUMBER, number);
+    Response response = target.request(MediaType.APPLICATION_JSON).get();
+    if (response.getStatus() == 400) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("getAggregatorList(..) failure! : " + errorMessage.getResult());
+      throw new InvalidArgumentsException(errorMessage.getResult());
+    }
+    List<Aggregator> subList = response.readEntity(new GenericType<List<Aggregator>>() {});
+    LOGGER.info("getAggregatorList(..) success!");
+
+    return subList;
+  }
+
+  /**
+   * Create an aggregator.
+   * 
+   * @param id
+   * @param name
+   * @param nameCode
+   * @param homepage
+   * @throws InvalidArgumentsException
+   * @throws MissingArgumentsException
+   * @throws AlreadyExistsException
+   * @throws InternalServerErrorException
+   */
+  public void createAggregator(String id, String name, String nameCode, String homepage)
+      throws InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      InternalServerErrorException {
+    WebTarget target = client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS);
+    Aggregator aggregator = new Aggregator(id, name, nameCode, homepage, null);
+    Response response =
+        target.request(MediaType.APPLICATION_JSON).post(
+            Entity.entity(aggregator, MediaType.APPLICATION_JSON), Response.class);
+
+    switch (response.getStatus()) {
+      case 400:
+        Result errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("createAggregator(..) failure! : " + errorMessage.getResult());
+        throw new InvalidArgumentsException(errorMessage.getResult());
+      case 406:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("createAggregator(..) failure! : " + errorMessage.getResult());
+        throw new MissingArgumentsException(errorMessage.getResult());
+      case 409:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("createAggregator(..) failure! : " + errorMessage.getResult());
+        throw new AlreadyExistsException(errorMessage.getResult());
+      case 500:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("createAggregator(..) failure! : " + errorMessage.getResult());
+        throw new InternalServerErrorException(errorMessage.getResult());
+    }
+    LOGGER.info("createAggregator(..) success!");
+  }
+
+  /**
+   * Update an aggregator by specifying the Id. Aggregator Id can be null if there is no need to
+   * change the id.
+   * 
+   * @param id
+   * @param newId
+   * @param name
+   * @param nameCode
+   * @param homepage
+   * @throws InvalidArgumentsException
+   * @throws MissingArgumentsException
+   * @throws DoesNotExistException
+   * @throws InternalServerErrorException
+   */
+  public void updateAggregator(String id, String newId, String name, String nameCode,
+      String homepage) throws InvalidArgumentsException, MissingArgumentsException,
+      DoesNotExistException, InternalServerErrorException {
+    WebTarget target =
+        client.target(restUrl + "/" + AggregatorOptionListContainer.AGGREGATORS + "/" + id);
+    Aggregator aggregator = new Aggregator(newId, name, nameCode, homepage, null);
+    Response response =
+        target.request(MediaType.APPLICATION_JSON).put(
+            Entity.entity(aggregator, MediaType.APPLICATION_JSON), Response.class);
+
+    switch (response.getStatus()) {
+      case 400:
+        Result errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("updateAggregator(..) failure! : " + errorMessage.getResult());
+        throw new InvalidArgumentsException(errorMessage.getResult());
+      case 404:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("updateAggregator(..) failure! : " + errorMessage.getResult());
+        throw new DoesNotExistException(errorMessage.getResult());
+      case 406:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("updateAggregator(..) failure! : " + errorMessage.getResult());
+        throw new MissingArgumentsException(errorMessage.getResult());
+      case 500:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("updateAggregator(..) failure! : " + errorMessage.getResult());
+        throw new InternalServerErrorException(errorMessage.getResult());
+    }
+    LOGGER.info("updateAggregator(..) success!");
   }
 
   public URL getRestUrl() {
--- client/client-rest-jersey/src/test/java/eu/europeana/repox/rest/client/accessors/AggregatorAccessorTest.java
+++ client/client-rest-jersey/src/test/java/eu/europeana/repox/rest/client/accessors/AggregatorAccessorTest.java
@@ -1,27 +1,29 @@
 /*
  * Copyright 2007-2015 The Europeana Foundation
- *
- *  Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved 
- *  by the European Commission;
- *  You may not use this work except in compliance with the Licence.
- *  
- *  You may obtain a copy of the Licence at:
- *  http://joinup.ec.europa.eu/software/page/eupl
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under 
- *  the Licence is distributed on an "AS IS" basis, without warranties or conditions of 
- *  any kind, either express or implied.
- *  See the Licence for the specific language governing permissions and limitations under 
- *  the Licence.
+ * 
+ * Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved by the
+ * European Commission; You may not use this work except in compliance with the Licence.
+ * 
+ * You may obtain a copy of the Licence at: http://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence
+ * is distributed on an "AS IS" basis, without warranties or conditions of any kind, either express
+ * or implied. See the Licence for the specific language governing permissions and limitations under
+ * the Licence.
  */
 package eu.europeana.repox.rest.client.accessors;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -32,7 +34,10 @@ import org.mockito.Mockito;
 import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
 
 import pt.utl.ist.dataProvider.Aggregator;
+import pt.utl.ist.util.exceptions.AlreadyExistsException;
 import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+import pt.utl.ist.util.exceptions.MissingArgumentsException;
 
 /**
  * @author Simon Tzanakis (Simon.Tzanakis@theeuropeanlibrary.org)
@@ -56,31 +61,163 @@ public class AggregatorAccessorTest {
     builder = Mockito.mock(Builder.class);
     response = Mockito.mock(Response.class);
     aa = new AggregatorAccessor(restUrl, username, password, client);
-  }
-  
-  @Test
-  public void testGetAggregator() {
+
     Mockito.when(client.target(Mockito.anyString())).thenReturn(webTarget);
     Mockito.when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
+    Mockito.when(webTarget.queryParam(Mockito.anyString(), Mockito.anyObject())).thenReturn(
+        webTarget);
     Mockito.when(builder.get()).thenReturn(response);
-    Mockito.when(response.getStatus()).thenReturn(404).thenReturn(200);
-    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Does not exist!"));
-    Mockito.when(response.readEntity(Aggregator.class)).thenReturn(new Aggregator());
+    Mockito.when(builder.delete()).thenReturn(response);
+    Mockito.when(
+        builder.post(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
+            Mockito.any(Class.class))).thenReturn(response);
+    Mockito.when(
+        builder.put(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
+            Mockito.any(Class.class))).thenReturn(response);
+  }
 
-    //First call get exception
-    Aggregator aggregator = null;
-    try {
-      aggregator = aa.getAggregator("A0r0");
-    } catch (DoesNotExistException e) {
-    }
-    Assert.assertNull(aggregator);
-    
-    aggregator = null;
-    try {
-      aggregator = aa.getAggregator("A0r0");
-    } catch (DoesNotExistException e1) {
-    }
+  // Tests for GetAggregator
+  @Test
+  public void testGetAggregator() throws DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(200);
+    Mockito.when(response.readEntity(Aggregator.class)).thenReturn(new Aggregator());
+    Aggregator aggregator = aa.getAggregator("A0r0");
     Assert.assertNotNull(aggregator);
   }
 
+  @Test(expected = DoesNotExistException.class)
+  public void testGetAggregatorDoesNotExist() throws DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(404);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Does not exist!"));
+
+    // First call get exception
+    Aggregator aggregator = null;
+    aggregator = aa.getAggregator("A0r0");
+    Assert.assertNull(aggregator);
+  }
+
+  // Tests for DeleteAggregator
+  @Test
+  public void testDeleteAggregator() throws DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(200);
+    aa.deleteAggregator("A0r0");
+  }
+
+  @Test(expected = DoesNotExistException.class)
+  public void testDeleteAggregatorInternalServerError() throws DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(404);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Does not exist!"));
+    aa.deleteAggregator("A0r0");
+  }
+
+  @Test(expected = InternalServerErrorException.class)
+  public void testDeleteAggregatorDoesNotExist() throws DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(500);
+    Mockito.when(response.readEntity(Result.class))
+        .thenReturn(new Result("Internal Server Error!"));
+    aa.deleteAggregator("A0r0");
+  }
+
+  // Tests for GetAggregatorList
+  @Test
+  public void testGetAggregatorList() throws InvalidArgumentsException {
+    Mockito.when(response.getStatus()).thenReturn(200);
+    aa.getAggregatorList(0, 1);
+    Mockito.when(response.readEntity(new GenericType<List<Aggregator>>() {})).thenReturn(
+        new ArrayList<Aggregator>());
+  }
+
+  @Test(expected = InvalidArgumentsException.class)
+  public void testGetAggregatorListInvalidArguments() throws InvalidArgumentsException {
+    Mockito.when(response.getStatus()).thenReturn(400);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Invalid argument!"));
+    aa.getAggregatorList(0, 1);
+  }
+
+  // Tests for CreateAggregator
+  @Test
+  public void testCreateAggregator() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(201);
+    aa.createAggregator("Id", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = InvalidArgumentsException.class)
+  public void testCreateAggregatorInvalidArguments() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(400);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Invalid argument!"));
+    aa.createAggregator("Id", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = MissingArgumentsException.class)
+  public void testCreateAggregatorMissingArguments() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(406);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Invalid argument!"));
+    aa.createAggregator("Id", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = AlreadyExistsException.class)
+  public void testCreateAggregatorAlreadyExists() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(409);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Already exist!"));
+    aa.createAggregator("Id", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = InternalServerErrorException.class)
+  public void testCreateAggregatorInternalServerError() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(500);
+    Mockito.when(response.readEntity(Result.class))
+        .thenReturn(new Result("Internal Server Error!"));
+    aa.createAggregator("Id", "Greece", "GR", "http://somepage.com");
+  }
+
+  // Tests for UpdateAggregator
+  @Test
+  public void testUpdateAggregator() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(201);
+    aa.updateAggregator("Id", "newId", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = InvalidArgumentsException.class)
+  public void testUpdateAggregatorInvalidArguments() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(400);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Invalid argument!"));
+    aa.updateAggregator("Id", "newId", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = DoesNotExistException.class)
+  public void testUpdateAggregatorAlreadyExists() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(404);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Already exist!"));
+    aa.updateAggregator("Id", "newId", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = MissingArgumentsException.class)
+  public void testUpdateAggregatorMissingArguments() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(406);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Invalid argument!"));
+    aa.updateAggregator("Id", "newId", "Greece", "GR", "http://somepage.com");
+  }
+
+  @Test(expected = InternalServerErrorException.class)
+  public void testUpdateAggregatorInternalServerError() throws InternalServerErrorException,
+      InvalidArgumentsException, MissingArgumentsException, AlreadyExistsException,
+      DoesNotExistException {
+    Mockito.when(response.getStatus()).thenReturn(500);
+    Mockito.when(response.readEntity(Result.class))
+        .thenReturn(new Result("Internal Server Error!"));
+    aa.updateAggregator("Id", "newId", "Greece", "GR", "http://somepage.com");
+  }
 }
