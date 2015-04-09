cef2164612ccde22e67fadf4774f2632600268bb
stzanakis <Simon.Tzanakis@theeuropeanlibrary.org>
2015-04-09 12:38:44 +0200

    Add startHarvest method with tests

--- client/client-rest-jersey/src/main/java/eu/europeana/repox/rest/client/accessors/HarvestAccessor.java
+++ client/client-rest-jersey/src/main/java/eu/europeana/repox/rest/client/accessors/HarvestAccessor.java
@@ -1,30 +1,40 @@
 /*
  * Copyright 2007-2015 The Europeana Foundation
- *
- *  Licensed under the EUPL, Version 1.1 (the "License") and subsequent versions as approved 
- *  by the European Commission;
- *  You may not use this work except in compliance with the License.
- *  
- *  You may obtain a copy of the License at:
- *  http://joinup.ec.europa.eu/software/page/eupl
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under 
- *  the License is distributed on an "AS IS" basis, without warranties or conditions of 
- *  any kind, either express or implied.
- *  See the License for the specific language governing permissions and limitations under 
- *  the License.
+ * 
+ * Licensed under the EUPL, Version 1.1 (the "License") and subsequent versions as approved by the
+ * European Commission; You may not use this work except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at: http://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" basis, without warranties or conditions of any kind, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package eu.europeana.repox.rest.client.accessors;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.theeuropeanlibrary.repox.rest.pathOptions.DatasetOptionListContainer;
+import org.theeuropeanlibrary.repox.rest.pathOptions.HarvestOptionListContainer;
+import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
+
+import pt.utl.ist.util.exceptions.AlreadyExistsException;
+import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+import pt.utl.ist.util.exceptions.MissingArgumentsException;
 
 /**
  * Access functionality to the harvests.
@@ -75,11 +85,50 @@ public class HarvestAccessor {
     client.register(authfeature);
     LOGGER.info("HarvestAccessor created with target url: {}", this.restUrl);
   }
-  
-  
-  
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+
+  /**
+   * Initiates a new harvest of the dataset with id.
+   * 
+   * @param id
+   * @param type
+   * @throws AlreadyExistsException
+   * @throws DoesNotExistException
+   */
+  public void startHarvest(String id, String type) throws AlreadyExistsException,
+      DoesNotExistException {
+    WebTarget target =
+        client.target(
+            restUrl + "/" + DatasetOptionListContainer.DATASETS + "/" + id + "/"
+                + HarvestOptionListContainer.HARVEST + "/" + HarvestOptionListContainer.START)
+            .queryParam(HarvestOptionListContainer.TYPE, type);
+    Response response =
+        target.request(MediaType.APPLICATION_JSON).post(
+            Entity.entity(null, MediaType.APPLICATION_JSON), Response.class);
+
+    switch (response.getStatus()) {
+      case 404:
+        Result errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("startHarvest(..) failure! : " + errorMessage.getResult());
+        throw new DoesNotExistException(errorMessage.getResult());
+      case 409:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("startHarvest(..) failure! : " + errorMessage.getResult());
+        throw new AlreadyExistsException(errorMessage.getResult());
+      case 500:
+        errorMessage = response.readEntity(Result.class);
+        LOGGER.warn("startHarvest(..) failure! : " + errorMessage.getResult());
+        throw new InternalServerErrorException(errorMessage.getResult());
+    }
+    Result errorMessage = response.readEntity(Result.class);
+    LOGGER.info("startHarvest(..) success! : " + errorMessage.getResult());
+  }
+
+
+  public static void main(String[] args) throws MalformedURLException, AlreadyExistsException,
+      DoesNotExistException {
+    HarvestAccessor ha =
+        new HarvestAccessor(new URL("http://localhost:8080/repox/rest"), "temporary", "temporary");
+    ha.startHarvest("a0660", HarvestOptionListContainer.FULL);
 
   }
 
--- client/client-rest-jersey/src/test/java/eu/europeana/repox/rest/client/accessors/HarvestAccessorTest.java
+++ client/client-rest-jersey/src/test/java/eu/europeana/repox/rest/client/accessors/HarvestAccessorTest.java
@@ -19,13 +19,27 @@ package eu.europeana.repox.rest.client.accessors;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.mockito.Mockito;
+import org.theeuropeanlibrary.repox.rest.pathOptions.HarvestOptionListContainer;
+import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
+
+import pt.utl.ist.dataProvider.DataSourceContainer;
+import pt.utl.ist.dataProvider.DefaultDataSourceContainer;
+import pt.utl.ist.util.exceptions.AlreadyExistsException;
+import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+import pt.utl.ist.util.exceptions.MissingArgumentsException;
 
 /**
  * @author Simon Tzanakis (Simon.Tzanakis@theeuropeanlibrary.org)
@@ -39,7 +53,7 @@ public class HarvestAccessorTest {
   private static WebTarget webTarget;
   private static Builder builder;
   private static Response response;
-  private static DatasetsAccessor da;
+  private static HarvestAccessor ha;
 
   @BeforeClass
   public static void setUpBeforeClass() throws IOException {
@@ -48,19 +62,49 @@ public class HarvestAccessorTest {
     webTarget = Mockito.mock(WebTarget.class);
     builder = Mockito.mock(Builder.class);
     response = Mockito.mock(Response.class);
-    da = new DatasetsAccessor(restUrl, username, password, client);
+    ha = new HarvestAccessor(restUrl, username, password, client);
 
-//    Mockito.when(client.target(Mockito.anyString())).thenReturn(webTarget);
-//    Mockito.when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
-//    Mockito.when(webTarget.queryParam(Mockito.anyString(), Mockito.anyObject())).thenReturn(
-//        webTarget);
+    Mockito.when(client.target(Mockito.anyString())).thenReturn(webTarget);
+    Mockito.when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
+    Mockito.when(webTarget.queryParam(Mockito.anyString(), Mockito.anyObject())).thenReturn(
+        webTarget);
 //    Mockito.when(builder.get()).thenReturn(response);
 //    Mockito.when(builder.delete()).thenReturn(response);
-//    Mockito.when(
-//        builder.post(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
-//            Mockito.any(Class.class))).thenReturn(response);
+    Mockito.when(
+        builder.post(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
+            Mockito.any(Class.class))).thenReturn(response);
 //    Mockito.when(
 //        builder.put(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
 //            Mockito.any(Class.class))).thenReturn(response);
+  }
+  
+  // Tests for StartHarvest
+  @Test
+  public void testStartHarvest() throws AlreadyExistsException, DoesNotExistException{
+    Mockito.when(response.getStatus()).thenReturn(200);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Success!"));
+    ha.startHarvest("ds0", HarvestOptionListContainer.SAMPLE);
+  }
+
+  @Test(expected = DoesNotExistException.class)
+  public void testStartHarvestDoesNotExist() throws DoesNotExistException, AlreadyExistsException {
+    Mockito.when(response.getStatus()).thenReturn(404);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Does not exist!"));
+    ha.startHarvest("ds0", HarvestOptionListContainer.SAMPLE);
+  }
+  
+  @Test(expected = AlreadyExistsException.class)
+  public void testStartHarvestAlreadyExists() throws AlreadyExistsException, DoesNotExistException{
+    Mockito.when(response.getStatus()).thenReturn(409);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Already exist!"));
+    ha.startHarvest("ds0", HarvestOptionListContainer.SAMPLE);
+  }
+
+  @Test(expected = InternalServerErrorException.class)
+  public void testStartHarvestInternalServerError() throws AlreadyExistsException, DoesNotExistException{
+    Mockito.when(response.getStatus()).thenReturn(500);
+    Mockito.when(response.readEntity(Result.class))
+        .thenReturn(new Result("Internal Server Error!"));
+    ha.startHarvest("ds0", HarvestOptionListContainer.SAMPLE);
   }
 }
