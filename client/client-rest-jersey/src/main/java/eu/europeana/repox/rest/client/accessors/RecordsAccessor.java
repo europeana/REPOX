3f3b8beb0c75006b24db5086f3d8ebf656bf2bcb
stzanakis <Simon.Tzanakis@theeuropeanlibrary.org>
2015-04-13 11:46:49 +0200

    Add RecordsAccessor initial classes commit with getRecord and tests

---
+++ client/client-rest-jersey/src/main/java/eu/europeana/repox/rest/client/accessors/RecordsAccessor.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2007-2015 The Europeana Foundation
+ * 
+ * Licensed under the EUPL, Version 1.1 (the "License") and subsequent versions as approved by the
+ * European Commission; You may not use this work except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at: http://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" basis, without warranties or conditions of any kind, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package eu.europeana.repox.rest.client.accessors;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.theeuropeanlibrary.repox.rest.pathOptions.RecordOptionListContainer;
+import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
+
+import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+
+/**
+ * @author Simon Tzanakis (Simon.Tzanakis@theeuropeanlibrary.org)
+ * @since Apr 13, 2015
+ */
+public class RecordsAccessor {
+  private URL restUrl;
+  private Client client = JerseyClientBuilder.newClient();
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatasetsAccessor.class);
+
+  /**
+   * Setup RecordsAccessor with the target Url, username and password
+   * 
+   * @param restUrl
+   * @throws MalformedURLException
+   */
+  public RecordsAccessor(URL restUrl, String username, String password)
+      throws MalformedURLException {
+    super();
+
+    this.restUrl =
+        restUrl.toString().charAt(restUrl.toString().length() - 1) == '/' ? new URL(restUrl
+            .toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
+    HttpAuthenticationFeature authfeature = HttpAuthenticationFeature.basic(username, password);
+    client.register(authfeature);
+    LOGGER.info("RecordsAccessor created with target url: {}", this.restUrl);
+  }
+
+  /**
+   * Only used for tests. Setup RecordsAccessor with the target Url, username and password
+   * 
+   * @param restUrl
+   * @param username
+   * @param password
+   * @param target
+   * @throws MalformedURLException
+   */
+  RecordsAccessor(URL restUrl, String username, String password, Client client)
+      throws MalformedURLException {
+    super();
+    this.restUrl =
+        restUrl.toString().charAt(restUrl.toString().length() - 1) == '/' ? new URL(restUrl
+            .toString().substring(0, restUrl.toString().length() - 1)) : restUrl;
+    this.client = client;
+    HttpAuthenticationFeature authfeature = HttpAuthenticationFeature.basic(username, password);
+    client.register(authfeature);
+    LOGGER.info("RecordsAccessor created with target url: {}", this.restUrl);
+  }
+
+  /**
+   * Retrieve the record with the provided id.
+   * @param id
+   * @return String with the record in xml
+   * @throws InvalidArgumentsException
+   * @throws DoesNotExistException
+   * @throws InternalServerErrorException
+   */
+  public String getRecord(String id) throws InvalidArgumentsException, DoesNotExistException,
+      InternalServerErrorException {
+    WebTarget target =
+        client.target(restUrl + "/" + RecordOptionListContainer.RECORDS).queryParam("recordId", id);
+    Response response = target.request(MediaType.APPLICATION_JSON).get();
+
+    if (response.getStatus() == 400) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("getRecord(..) failure! : " + errorMessage.getResult());
+      throw new InvalidArgumentsException(errorMessage.getResult());
+    } else if (response.getStatus() == 404) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("getRecord(..) failure! : " + errorMessage.getResult());
+      throw new DoesNotExistException(errorMessage.getResult());
+    } else if (response.getStatus() == 500) {
+      Result errorMessage = response.readEntity(Result.class);
+      LOGGER.warn("getRecord(..) failure! : " + errorMessage.getResult());
+      throw new InternalServerErrorException(errorMessage.getResult());
+    }
+    LOGGER.info("getRecord(..) success!");
+    Result result = response.readEntity(Result.class);
+    return result.getResult();
+  }
+
+  public static void main(String[] args) throws MalformedURLException, InternalServerErrorException, InvalidArgumentsException, DoesNotExistException, UnsupportedEncodingException, FileNotFoundException {
+    RecordsAccessor ra =
+        new RecordsAccessor(new URL("http://localhost:8080/repox/rest"), "temporary", "temporary");
+    
+//    System.out.println(ra.getRecord("oai:the.european.library.a0660:urn:onb.ac.at:abo:http://data.onb.ac.at/ABO/%2BZ103526808"));
+    
+    String s = ra.getRecord("oai:the.european.library.a0660:urn:onb.ac.at:abo:http://data.onb.ac.at/ABO/%252BZ103526808");
+    System.out.println(s);
+  }
+
+}
---
+++ client/client-rest-jersey/src/test/java/eu/europeana/repox/rest/client/accessors/RecordsAccessorTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2007-2015 The Europeana Foundation
+ * 
+ * Licensed under the EUPL, Version 1.1 (the "License") and subsequent versions as approved by the
+ * European Commission; You may not use this work except in compliance with the License.
+ * 
+ * You may obtain a copy of the License at: http://joinup.ec.europa.eu/software/page/eupl
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" basis, without warranties or conditions of any kind, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package eu.europeana.repox.rest.client.accessors;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.theeuropeanlibrary.repox.rest.pathOptions.Result;
+
+import pt.utl.ist.util.exceptions.DoesNotExistException;
+import pt.utl.ist.util.exceptions.InvalidArgumentsException;
+
+/**
+ * @author Simon Tzanakis (Simon.Tzanakis@theeuropeanlibrary.org)
+ * @since Apr 13, 2015
+ */
+public class RecordsAccessorTest {
+  private static URL restUrl;
+  private static String username = "username";
+  private static String password = "password";
+  private static Client client;
+  private static WebTarget webTarget;
+  private static Builder builder;
+  private static Response response;
+  private static RecordsAccessor ra;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws IOException {
+    restUrl = new URL("http://examp.com/er");
+    client = Mockito.mock(Client.class);
+    webTarget = Mockito.mock(WebTarget.class);
+    builder = Mockito.mock(Builder.class);
+    response = Mockito.mock(Response.class);
+    ra = new RecordsAccessor(restUrl, username, password, client);
+
+    Mockito.when(client.target(Mockito.anyString())).thenReturn(webTarget);
+    Mockito.when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
+    Mockito.when(webTarget.queryParam(Mockito.anyString(), Mockito.anyObject())).thenReturn(
+        webTarget);
+    Mockito.when(builder.get()).thenReturn(response);
+    // Mockito.when(builder.delete()).thenReturn(response);
+    // Mockito.when(
+    // builder.post(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
+    // Mockito.any(Class.class))).thenReturn(response);
+    // Mockito.when(
+    // builder.put(Entity.entity(Mockito.any(Class.class), MediaType.APPLICATION_JSON),
+    // Mockito.any(Class.class))).thenReturn(response);
+  }
+
+  // Tests for GetRecord
+  @Test
+  public void testGetRecord() throws DoesNotExistException, InternalServerErrorException, InvalidArgumentsException {
+    Mockito.when(response.getStatus()).thenReturn(200);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("<root>xml record</root>"));
+    String record = ra.getRecord("recordId");
+    Assert.assertNotNull(record);
+  }
+
+  @Test(expected = DoesNotExistException.class)
+  public void testGetRecordDoesNotExist() throws DoesNotExistException, InternalServerErrorException, InvalidArgumentsException {
+    Mockito.when(response.getStatus()).thenReturn(404);
+    Mockito.when(response.readEntity(Result.class)).thenReturn(new Result("Does not exist!"));
+    ra.getRecord("recordId");
+  }
+
+  @Test(expected = InternalServerErrorException.class)
+  public void testGetRecordInternalServerError() throws DoesNotExistException, InternalServerErrorException, InvalidArgumentsException {
+    Mockito.when(response.getStatus()).thenReturn(500);
+    Mockito.when(response.readEntity(Result.class))
+        .thenReturn(new Result("Internal Server Error!"));
+    ra.getRecord("recordId");
+  }
+
+
+
+}
